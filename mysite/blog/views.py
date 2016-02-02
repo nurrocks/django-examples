890a3c6e8e059a021c2579d2811f62f08398e7af
zhexiao <zhexiao@163.com>
2016-02-02 04:58:46 +0000

    add pahe

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
+from django.views.generic import ListView
+
+# def post_list(request):
+#     object_list = Post.published.all()
+#     paginator = Paginator(object_list, 2) # 3 posts in each page
+#     page = request.GET.get('page')
+
+#     try:
+#     	posts = paginator.page(page)
+#     except PageNotAnInteger:
+#     	posts = paginator.page(1)
+#     except EmptyPage:
+#     	posts = paginator.page(paginator.num_pages)
+
+#     return render(request, 'blog/post/list.html', {
+#     	'posts': posts
+#     })
+
+
+class PostListView(ListView):
+	queryset = Post.published.all()
+	context_object_name = 'posts'
+	paginate_by = 2
+	template_name = 'blog/post/list.html'
 
 
 def post_detail(request, year, month, day, post):
