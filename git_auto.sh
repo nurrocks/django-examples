93c5a354a1b4876fd1f3a35ad8c8c24b3e8284db
zhexiao <zhexiao@163.com>
2016-04-27 09:22:44 -0400

    a

---
+++ git_auto.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+
+# loop
+while true
+do
+# pers=$(stat -c %A README.md)
+# if [ "$pers"  = "-rwxrwxrwx" ]; then  
+#     eval "chmod 555 README.md"
+# else
+#     eval "chmod 777 README.md"
+# fi
+
+echo 1 >> README.md
+eval "git commit -a -m 'change README'"
+eval "git push"
+
+# exec this script each 6000 seconds
+sleep 24000
+done
